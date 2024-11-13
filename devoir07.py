fb96a0399caad2ebbf93d16c90765a9935ce82ec
ixe013 <guillaume@paralint.com>
2024-11-12 21:17:54 -0500

    Worked enought to grade

--- devoir07.py
+++ devoir07.py
@@ -1,21 +1,44 @@
 #!python3
 
 import base64
+import binascii
+import re
 import sys
 
 import LnkParse3
 
-# Lire le contenu Base64 depuis stdin
-base64_string = sys.stdin.read()
+base64line = re.compile("^[a-zA-Z0-9+/]+(={0,3})?")
 
-# Décoder le contenu
-decoded_bytes = base64.b64decode(base64_string.replace('\n', ''))
-#print(decoded_bytes)
+def hexdump(data: bytes):
+    def to_printable_ascii(byte):
+        return chr(byte) if 32 <= byte <= 126 else "."
 
-#exit()
+    offset = 0
+    while offset < len(data):
+        chunk = data[offset : offset + 16]
+        hex_values = " ".join(f"{byte:02x}" for byte in chunk)
+        ascii_values = "".join(to_printable_ascii(byte) for byte in chunk)
+        print(f"{offset:08x}  {hex_values:<48}  {ascii_values}")
+        offset += 16
 
-#indata = ''.join([line.strip() for line in sys.stdin.readlines()])
 
-#lnk = LnkParse3.lnk_file(indata)
-lnk = LnkParse3.lnk_file(indata = decoded_bytes)
-lnk.print_shortcut_target()
+try:
+    # Lire le contenu Base64 depuis stdin
+    base64_string = sys.stdin.read()
+    # Décoder le contenu
+    decoded_bytes = base64.b64decode(base64_string.replace('\n', ''))
+    #lnk = LnkParse3.lnk_file(indata)
+    lnk = LnkParse3.lnk_file(indata = decoded_bytes)
+    target = lnk.print_shortcut_target()
+
+
+    if not target:
+        print("Curated base64:")
+        print(base64_string.replace('\n', ''))
+        lnk.print_lnk_file()
+
+    hexdump(decoded_bytes)
+    print(f"Links to {target}")
+    exit(0)
+except KeyboardInterrupt:
+    exit(1)
